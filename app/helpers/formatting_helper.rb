e94690195901e6e8ad85f68eb6ca0bd0d53bb571
EqualMa <equalma@outlook.com>
2022-04-12 17:55:12 +0000

    custom-fix: re-apply status-content-type-html for mastodon v3.5.1

--- app/helpers/formatting_helper.rb
+++ app/helpers/formatting_helper.rb
@@ -15,6 +15,10 @@ module FormattingHelper
   module_function :extract_status_plain_text
 
   def status_content_format(status)
+    if status.content_type == 'text/html'
+      return status.text
+    end
+
     html_aware_format(status.text, status.local?, preloaded_accounts: [status.account] + (status.respond_to?(:active_mentions) ? status.active_mentions.map(&:account) : []))
   end
 
